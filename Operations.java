c90cea3bd26d376c807762883557be879702357b
Kritikab9 <56131332+Kritikab9@users.noreply.github.com>
2020-08-15 01:34:54 +0530

    java file added

---
+++ Operations.java
@@ -0,0 +1,29 @@
+
+package newproject;
+
+public class Operations
+{
+     public int add(int a,int b)
+    {
+        return a+b;
+    }
+    
+    public int sub(int a ,int b)
+    {
+        return a-b;
+    }
+   
+    public int mult(int a, int b)
+    {
+        return a*b;
+    
+    }
+     public int div(int a,int b)
+     {
+         return a/b;
+     }
+        
+        
+   
+    
+}
